9b5aa82d313e04850f6e8765a61b0a8e0a2d2fe5
Juan Torres <tsjuancamilo@gmail.com>
2017-06-12 21:51:34 -0500

    Name fixes and starting project

--- warepathlol/wsgi.py
+++ warepathlol/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LolPath project.
+WSGI config for warepathlol project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LolPath.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warepathlol.settings")
 
 application = get_wsgi_application()
